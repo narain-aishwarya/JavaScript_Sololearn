287dff8b6d90b7965f3d86f965c7746ee98d001b
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2024-03-25 23:43:48 +0530

    Add files via upload

---
+++ animation/script.js
@@ -0,0 +1,17 @@
+var pos = 0;
+
+//box element 
+var box = document.getElementById("box");
+var t = setInterval(move, 10);
+
+function move(){
+    if (pos >= 150){
+         clearInterval(t);
+    }
+
+    else {
+        pos += 1;
+
+        box.style.left = pos + 'px'; 
+    }
+}
